701ca446a1d5a19e6aa67ffb6772d6090122c98e
Mahmut Serdar İset <mahmutserdarist@gmail.com>
2023-02-14 16:41:57 +0300

    responsive

--- components/Main/CategoryButton.vue
+++ components/Main/CategoryButton.vue
@@ -4,7 +4,7 @@
       <img :src="require(`~/assets/img/${category.img}.png`)" alt="" />
     </div>
     <span class="category-name">
-        {{ category.name }}
+      {{ category.name }}
     </span>
   </div>
 </template>
@@ -36,19 +36,27 @@ export default {
   font-size: 3rem;
   color: #fff;
   border-radius: 10px;
+  @include media(xs, sm) {
+    width: 65px;
+    height: 65px;
+  }
   img {
     max-width: 40%;
   }
 }
 .category-name {
-    width: 125px;
-    text-align: center;
-    display: flex;
-    justify-content: center;
-    margin-top: 0.5rem;
-    color: $primary-color;
-    font-weight: 600;
-    word-break: break-word;
-    overflow: hidden;
+  width: 125px;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  margin-top: 0.5rem;
+  color: $primary-color;
+  font-weight: 600;
+  word-break: break-word;
+  overflow: hidden;
+  @include media(xs, sm) {
+    width: 65px;
+    font-size: 0.825rem;
+  }
 }
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -28,7 +28,7 @@ export default {
   computed: {
     token() {
       return this.$store.state.user.token
-    }
+    },
   },
   async mounted() {
     await this.$store.dispatch('advert/fetchAdverts')
@@ -78,18 +78,37 @@ a {
     width: 100%;
     background-color: #fff;
     border-left: 1px solid #dedede;
+    position: relative;
+    @include media(xs, sm) {
+      flex-direction: column-reverse;
+    }
 
     .side-bar-section {
       width: 250px;
       height: 100%;
       position: relative;
       background-color: #fff;
+      @include media(xs, sm) {
+        width: 100%;
+        position: fixed;
+        bottom: 0;
+        left: 0;
+        z-index: 100;
+        height: 3rem;
+        box-shadow: 0px -2px 10px 0px rgba(0, 0, 0, 0.1);
+      }
+
     }
     .content-section {
       width: calc(100% - (250px));
       background-color: #fff;
       border-left: 1px solid #dedede;
       border-right: 1px solid #dedede;
+      @include media(xs, sm) {
+        width: 100%;
+        border-left: none;
+        border-right: none;
+      }
       .content-wrapper {
         height: 100%;
         background-color: #fff;
